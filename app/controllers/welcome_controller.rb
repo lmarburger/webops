842ca96ef85799102a3c036db6c937d5a14f20b1
Larry Marburger <larry@marburger.cc>
2015-05-15 14:56:39 -0400

    Organize gem dependencies

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,12 @@
 class WelcomeController < ApplicationController
+  def index
+  end
+
+  private
+
+  helper_method :incidents
+
+  def incidents
+    pagerduty = Pagerduty.new("1W7HnVqvp7z2ZsBd71fy")
+  end
 end
